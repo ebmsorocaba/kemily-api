09688d1e0c424413946ba9d2f76617d592933f5c
Diego Baptista <diegoluizbaptista@gmail.com>
2017-04-25 19:49:08 -0300

    first commit

---
+++ src/app/index.module.js
@@ -0,0 +1,26 @@
+(function ()
+{
+    'use strict';
+
+    /**
+     * Main module of the Fuse
+     */
+    angular
+        .module('fuse', [
+
+            // Core
+            'app.core',
+
+            // Navigation
+            'app.navigation',
+
+            // Toolbar
+            'app.toolbar',
+
+            // Quick Panel
+            'app.quick-panel',
+
+            // Sample
+            'app.sample'
+        ]);
+})();
