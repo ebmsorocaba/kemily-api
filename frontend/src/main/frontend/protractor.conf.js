b52e6880df166c8fafb72c91491bc08d73590ee8
Diego Baptista <diegoluizbaptista@gmail.com>
2017-04-30 01:45:25 -0300

    frontend + backend working in same project

--- frontend/src/main/frontend/protractor.conf.js
+++ frontend/src/main/frontend/protractor.conf.js
@@ -13,7 +13,7 @@ exports.config = {
     'browserName': 'chrome'
   },
 
-  baseUrl: 'http://localhost:80',
+  baseUrl: 'http://localhost:3000',
 
   // Spec patterns are relative to the current working directory when
   // protractor is called.
