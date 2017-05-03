121d1c98e6ac1e2c84642bf5c0e69818e517af66
Douglas Fernandes <douglas16.2009@gmail.com>
2017-05-02 23:04:19 -0300

    Non-functional front-end for Usuários and Associados

--- frontend/src/main/frontend/src/app/main/associado/associado.module.js
+++ frontend/src/main/frontend/src/app/main/associado/associado.module.js
@@ -19,37 +19,37 @@
                         controller : 'AssociadoController as vm'
                     }
                 },
-                resolve: {
-                    SampleData: function (msApi)
-                    {
-                        return msApi.resolve('sample@get');
-                    }
-                }
+                // resolve: {
+                //     SampleData: function (msApi)
+                //     {
+                //         return msApi.resolve('sample@get');
+                //     }
+                // }
             });
 
         // Translation
         // Usado no sistema de múltiplos idiomas. Indica a pasta onde fica a pasta com os JSONs de Strings. Veja: \index.config.js
-        $translatePartialLoaderProvider.addPart('app/main/associado');
+        // $translatePartialLoaderProvider.addPart('app/main/associado');
 
         // Api
-        msApiProvider.register('sample', ['app/data/sample/sample.json']);
+        //msApiProvider.register('sample', ['app/data/sample/sample.json']);
 
         // Navigation
         // Esta seção controla como este módulo aparece no menu.
         msNavigationServiceProvider.saveItem('financeiro', { // Adiciona um item no menu
-            title : 'ASSOCIADO.ASSOCIADO_MENU', // Nome do item/grupo no menu. Veja: [...]\associado\i18n
+            title : 'Financeiro', // Nome do item/grupo no menu. Veja: [...]\associado\i18n
             group : true, // Define se é um item [false] ou grupo de itens [true].
-            weight: 2 // Ordem no menu. Baseado em prioridade.
+            weight: 1 // Ordem no menu. Baseado em prioridade.
         });
 
         msNavigationServiceProvider.saveItem('financeiro.associado', { // Adiciona um sub-item dentro de [administrador].
-            title    : 'Associado',
+            title    : 'Gerenciar Associados',
             icon     : 'icon-tile-four', // Ícone deste item no menu.
             state    : 'app.associado', // Qual módulo o item aponta/abre.
             /*stateParams: {
                 'param1': 'page'
              },*/
-            translate: 'ASSOCIADO.ASSOCIADO_ITEM', // Usado no sistema de multiplos idiomas. Veja: [...]\usuario\i18n
+            //translate: 'ASSOCIADO.ASSOCIADO_ITEM', // Usado no sistema de multiplos idiomas. Veja: [...]\usuario\i18n
             weight   : 0 // Ordem no menu. Baseado em prioridade.
         });
     }
