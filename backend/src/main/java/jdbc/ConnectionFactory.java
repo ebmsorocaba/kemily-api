b993b9ffead1f2be24119109b6e4867cc2cc926b
Medson <medson.oliveira.jr@gmail.com>
2017-08-02 16:59:32 -0300

    Change connection string to default

--- backend/src/main/java/jdbc/ConnectionFactory.java
+++ backend/src/main/java/jdbc/ConnectionFactory.java
@@ -36,10 +36,10 @@ public class ConnectionFactory {
 
 			/** Localhost */
 
-			return DriverManager.getConnection("jdbc:postgresql://localhost:5432/kemily", "diego", "diego");
+			return DriverManager.getConnection("jdbc:postgresql://localhost:5432/ebm_admin", "postgres", "postgres");
 
 			/** Produção */
-			//return DriverManager.getConnection("jdbc:postgresql://localhost:5432/kemily", "postgres", "k3m1l1");
+			//return DriverManager.getConnection("jdbc:postgresql://localhost:5432/ebm_admin", "postgres", "postgres");
 
 		} catch (ClassNotFoundException e) {
 			throw new SQLException(e.getMessage());
