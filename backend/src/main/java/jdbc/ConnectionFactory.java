2eff2ab0865fb660d9765d050d3e12553518105b
Diego Ferreira <diegofs01@hotmail.com>
2017-08-22 12:57:24 -0300

    Change Pooling from 'ConnectionPool' to 'PoolingDataSource'

--- backend/src/main/java/jdbc/ConnectionFactory.java
+++ backend/src/main/java/jdbc/ConnectionFactory.java
@@ -6,11 +6,12 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 import org.postgresql.jdbc3.Jdbc3ConnectionPool;
+import org.postgresql.jdbc3.Jdbc3PoolingDataSource;
 
 public class ConnectionFactory {
 	
 	private static Connection conn;
-	private static Jdbc3ConnectionPool dataSource;
+	private static Jdbc3PoolingDataSource dataSource;
 	
 	/** NOTA DO DIEGO FERREIRA*/
 	
@@ -26,31 +27,31 @@ public class ConnectionFactory {
 	private static void createConnectionPool(String tipo) {
 		if(tipo.equals("localhost")) {
 			//LOCALHOST
-			Jdbc3ConnectionPool pool = new Jdbc3ConnectionPool();
+			Jdbc3PoolingDataSource pool = new Jdbc3PoolingDataSource();
 			pool.setUrl("jdbc:postgresql://localhost:5432/ebm_admin");
 			pool.setUser("postgres");
 			pool.setPassword("postgres");
-			
+			pool.setMaxConnections(15);
 			dataSource = pool;
 		} else {
 			if(tipo.equals("heroku-test")) {
 				//HEROKU TESTING
-				Jdbc3ConnectionPool pool = new Jdbc3ConnectionPool();
+				Jdbc3PoolingDataSource pool = new Jdbc3PoolingDataSource();
 				pool.setUrl("jdbc:postgresql://ec2-107-22-236-252.compute-1.amazonaws.com:5432/d3k8ui2hd4460h?sslmode=require");
 				pool.setUser("uimgbmczwnxbtx");
 				pool.setPassword("20bd33bf81cf0b1ec35371a0c742783c027b8c5ff856067b6a456501ae83c06e");
 				pool.setSsl(true);
-				
+				pool.setMaxConnections(15);
 				dataSource = pool;
 			} else {
 				if(tipo.equals("heroku-homologacao")) {
 					//HEROKU HOMOLOGAÇÃO
-					Jdbc3ConnectionPool pool = new Jdbc3ConnectionPool();
+					Jdbc3PoolingDataSource pool = new Jdbc3PoolingDataSource();
 					pool.setUrl("jdbc:postgresql://ec2-50-17-236-15.compute-1.amazonaws.com:5432/d18n1ki8qe2orj?sslmode=require");
 					pool.setUser("pjcvqvzaytfmmi");
 					pool.setPassword("9416bca22c6cf3392b8c9483de6c78cd52cf96b1d2f3504ea51de2eae260d9ba");
 					pool.setSsl(true);
-					
+					pool.setMaxConnections(15);
 					dataSource = pool;
 				}
 			}
