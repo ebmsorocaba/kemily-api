66a9609bd9eccfd0156541129ad1d38fd134f382
Medson <medson.oliveira.jr@gmail.com>
2017-09-28 15:46:38 -0300

    Remove useless methods

--- backend/src/main/java/controller/aluno/ImovelController.java
+++ backend/src/main/java/controller/aluno/ImovelController.java
@@ -17,42 +17,10 @@ import java.util.Map;
 
 @RestController
 public class ImovelController {
-    private Map<Integer, Imovel> imovels;
+
     private ImovelDAO imovelDao = new ImovelDAO();
 
-    public ImovelController() throws SQLException {
-        imovels = new HashMap<Integer, Imovel>();
-    }
-
-    @CrossOrigin
-    @RequestMapping(value = "/api/imovel", method = RequestMethod.GET)
-    public ResponseEntity<List<Imovel>> listar() throws SQLException {
-        int index=0;
-
-        List<Imovel> imovelsGetted = new ArrayList<Imovel>();
-        imovels = new HashMap<Integer, Imovel>();
-
-        imovelsGetted = imovelDao.getLista();
-
-        for (Imovel usu : imovelsGetted) {
-            imovels.put(index, usu);
-            index++;
-        }
-
-        return new ResponseEntity<List<Imovel>>(new ArrayList<Imovel>(imovels.values()), HttpStatus.OK);
-    }
-
-    @CrossOrigin
-    @RequestMapping(value = "/api/imovel/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Imovel> buscar(@PathVariable("id") int id) throws SQLException {
-
-        Imovel imovel = imovelDao.getImovel(id);
-        if (imovel == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-
-        return new ResponseEntity<Imovel>(imovel, HttpStatus.OK);
-    }
+    public ImovelController() throws SQLException { }
 
     @CrossOrigin
     @RequestMapping(value = "/api/imovel/{id}", method = RequestMethod.DELETE)
@@ -61,20 +29,4 @@ public class ImovelController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-
-    @CrossOrigin
-    @RequestMapping(value = "/api/imovel", method = RequestMethod.POST)
-    public ResponseEntity<Imovel> addImovel(@RequestBody Imovel imovel) throws JsonParseException, JsonMappingException, IOException, SQLException {
-
-        imovelDao.adiciona(imovel);
-        return new ResponseEntity<Imovel>(imovel, HttpStatus.CREATED);
-    }
-
-    @CrossOrigin
-    @RequestMapping(value = "/api/imovel/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<Imovel> updateImovel(@RequestBody Imovel imovel, @PathVariable("id") int id) throws JsonParseException, JsonMappingException, IOException, SQLException {
-
-        imovelDao.altera(imovel, id);
-        return new ResponseEntity<Imovel>(imovel, HttpStatus.CREATED);
-    }
 }
