bf76ab7b11e00a80d3aa37c49f5f153828a76ebd
Medson <medson.oliveira.jr@gmail.com>
2017-10-15 21:21:50 -0200

    Add convert methods to date and hour

--- frontend/src/main/frontend/src/app/main/historicoOcorrencia/dialogs/historicoOcorrencia/historicoOcorrencia-lista-dialog.controller.js
+++ frontend/src/main/frontend/src/app/main/historicoOcorrencia/dialogs/historicoOcorrencia/historicoOcorrencia-lista-dialog.controller.js
@@ -1,12 +1,25 @@
 (function() {
-  'use strict';
+  "use strict";
 
-  angular.module('app.historicoOcorrencia').controller('HistoricoOcorrenciaListaDialogController', HistoricoOcorrenciaListaDialogController)
+  angular
+  .module("app.historicoOcorrencia")
+  .controller(
+    "HistoricoOcorrenciaListaDialogController",
+    HistoricoOcorrenciaListaDialogController
+  );
 
   /** @ngInject */
-  function HistoricoOcorrenciaListaDialogController ($mdDialog, $state, Aluno, Ocorrencias, User, msUtils, api) {
+  function HistoricoOcorrenciaListaDialogController(
+    $mdDialog,
+    $state,
+    Aluno,
+    Ocorrencias,
+    User,
+    msUtils,
+    api
+  ) {
     var vm = this;
-    vm.title = 'Ocorrencias do Aluno(a) ' + Aluno.nome;
+    vm.title = "Ocorrencias do Aluno(a) " + Aluno.nome;
     vm.aluno = angular.copy(Aluno);
     vm.user = User;
     vm.allFields = false;
@@ -16,23 +29,38 @@
     vm.openManipularOcorrenciaDialog = openManipularOcorrenciaDialog;
     vm.closeDialog = closeDialog;
     vm.deletarTodasAsOcorrencias = deletarTodasAsOcorrencias;
+    vm.formataData = formataData;
+    vm.formataHora = formataHora;
+
+    function formataData(data) {
+      var aux = new Date(data);
+      aux.setDate(aux.getDate() + 2)
+      return aux.toLocaleDateString("pt-BR");
+    };
+
+    function formataHora(hora) {
+      var aux = hora
+      return aux.replace(/\:\d\d$/, '');
+    }
 
     function popularLista() {
-      var lista = [];
-      Ocorrencias.forEach(function(oco) {
-        if(vm.aluno.ra == oco.raAluno) {
-          lista.push(oco);
-        }
-      })
+      var lista = Ocorrencias.filter(function(oco) {
+        return vm.aluno.ra === oco.raAluno;
+      });
+
+      lista.forEach(function(elem) {
+        elem.dataSearch = formataData(elem.data);
+        elem.horaSearch = formataHora(elem.hora);
+      });
       return lista;
     }
 
-    function openManipularOcorrenciaDialog (ev, ocorrencia) {
+    function openManipularOcorrenciaDialog(ev, ocorrencia) {
       $mdDialog.show({
-        controller: 'ManipularOcorrenciaDialogController',
-        controllerAs: 'vm',
-        templateUrl: 'app/main/historicoOcorrencia/dialogs/historicoOcorrencia/historicoOcorrencia-manipular-dialog.html',
-        parent: (angular.element(document.body)),
+        controller: "ManipularOcorrenciaDialogController",
+        controllerAs: "vm",
+        templateUrl: "app/main/historicoOcorrencia/dialogs/historicoOcorrencia/historicoOcorrencia-manipular-dialog.html",
+        parent: angular.element(document.body),
         targetEvent: ev,
         clickOutsideToClose: true,
         locals: {
@@ -45,38 +73,47 @@
     }
 
     function deletarTodasAsOcorrencias(ev) {
-      var confirm = $mdDialog.confirm().title('Você tem certeza de que deseja apagar TODAS as ocorrencias do aluno?')
-                            .htmlContent('<b>As ocorrencias do ' + vm.aluno.nome + ' (' + vm.aluno.ra + ')</b>' + ' serão apagadas.')
-                            .ariaLabel('apagar todas as ocorrencias')
-                            .targetEvent(ev)
-                            .ok('Sim')
-                            .cancel('Cancelar');
+      var confirm = $mdDialog
+      .confirm()
+      .title(
+        "Você tem certeza de que deseja apagar TODAS as ocorrencias do aluno?"
+      )
+      .htmlContent(
+        "<b>As ocorrencias do " +
+        vm.aluno.nome +
+        " (" +
+        vm.aluno.ra +
+        ")</b>" +
+        " serão apagadas."
+      )
+      .ariaLabel("apagar todas as ocorrencias")
+      .targetEvent(ev)
+      .ok("Sim")
+      .cancel("Cancelar");
       $mdDialog.show(confirm).then(function() {
         api.historicoOcorrencia.getByAluno.delete({
-          'ra' : vm.aluno.ra
-          },
-          function(response) {
-            console.log(response);
-          },
-          // Erro
-          function(response) {
-            console.error(response);
-          }
-        );
+          ra: vm.aluno.ra
+        },
+        function(response) {
+          console.log(response);
+        },
+        // Erro
+        function(response) {
+          console.error(response);
+        }
+      );
 
-        closeDialog();
-
-      });
-    }
-
-    vm.routeReload = function() {
-      $state.reload()
-    }
-
-    function closeDialog() {
-      $mdDialog.hide();
-      vm.routeReload();
-    }
+      closeDialog();
+    });
   }
+
+  vm.routeReload = function() {
+    $state.reload();
+  };
+
+  function closeDialog() {
+    $mdDialog.hide();
+    vm.routeReload();
+  }
+}
 })();
-  
