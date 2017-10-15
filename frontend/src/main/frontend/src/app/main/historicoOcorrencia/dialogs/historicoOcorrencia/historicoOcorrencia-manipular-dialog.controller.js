e96f4510c341c58b874a5a863d21aba2ccf3274c
Medson <medson.oliveira.jr@gmail.com>
2017-10-15 21:22:45 -0200

    Undefine auxiliar variables

--- frontend/src/main/frontend/src/app/main/historicoOcorrencia/dialogs/historicoOcorrencia/historicoOcorrencia-manipular-dialog.controller.js
+++ frontend/src/main/frontend/src/app/main/historicoOcorrencia/dialogs/historicoOcorrencia/historicoOcorrencia-manipular-dialog.controller.js
@@ -4,14 +4,14 @@
   angular.module('app.historicoOcorrencia').controller('ManipularOcorrenciaDialogController', ManipularOcorrenciaDialogController)
 
   /** @ngInject */
-  function ManipularOcorrenciaDialogController ($mdDialog, $state, Ocorrencia, User, msUtils, Aluno, api) {
+  function ManipularOcorrenciaDialogController($mdDialog, $state, Ocorrencia, User, msUtils, Aluno, api) {
     var vm = this;
     vm.title = 'Manipular Ocorrencia';
     vm.ocorrencia = angular.copy(Ocorrencia);
     vm.user = User;
     vm.allFields = false;
 
-    if(!vm.ocorrencia) {
+    if (!vm.ocorrencia) {
       vm.ocorrencia = {
         'data': '',
         'hora': '',
@@ -22,10 +22,10 @@
     } else {
       //2017-10-14
       var aux = vm.ocorrencia.data;
-      vm.ocorrencia.data = aux.substring(8) + '/' + aux.substring(5,7) + '/' + aux.substring(0,5);
+      vm.ocorrencia.data = aux.substring(8) + '/' + aux.substring(5, 7) + '/' + aux.substring(0, 5);
       vm.title = 'Alterar Ocorrencia';
     }
-    
+
     if (Ocorrencia !== undefined) {
       vm.newOcorrencia = false
     } else {
@@ -41,18 +41,21 @@
       $state.reload()
     }
 
-    function closeDialog () {
+    function closeDialog() {
       $mdDialog.hide();
       vm.routeReload();
     }
 
     function salvarOcorrencia() {
       var aux = vm.ocorrencia.data;
-      vm.ocorrencia.data = aux.substring(6,10) + '-' + aux.substring(3,5) + '-' + aux.substring(0,2);
+      console.log(aux);
+      vm.ocorrencia.data = aux.substring(6, 10) + '-' + aux.substring(3, 5) + '-' + aux.substring(0, 2);
       vm.ocorrencia.hora += ':00';
+      vm.ocorrencia.dataSearch = undefined;
+      vm.ocorrencia.horaSearch = undefined;
 
       api.historicoOcorrencia.ocorrencia.update({
-        'ra': vm.ocorrencia.raAluno
+          'ra': vm.ocorrencia.raAluno
         }, vm.ocorrencia,
         // Exibe o resultado no console do navegador:
         // Sucesso
@@ -70,7 +73,7 @@
 
     function adicionarOcorrencia() {
       var aux = vm.ocorrencia.data;
-      vm.ocorrencia.data = aux.substring(6,10) + '-' + aux.substring(3,5) + '-' + aux.substring(0,2);  
+      vm.ocorrencia.data = aux.substring(6, 10) + '-' + aux.substring(3, 5) + '-' + aux.substring(0, 2);
       vm.ocorrencia.hora = vm.ocorrencia.hora + ':00';
 
       api.historicoOcorrencia.list.save(vm.ocorrencia,
@@ -90,21 +93,21 @@
 
     function deletarOcorrencia(ev, ocorrencia) {
       var confirm = $mdDialog.confirm().title('Você tem certeza de que deseja apagar a ocorrencia?')
-                            .htmlContent('<b>As ocorrencia com a data: ' + ocorrencia.data + ' ' + ocorrencia.hora + '</b> será apagada.')
-                            .ariaLabel('apagar ocorrencia')
-                            .targetEvent(ev)
-                            .ok('Sim')
-                            .cancel('Cancelar');
+        .htmlContent('<b>As ocorrencia com a data: ' + ocorrencia.data + ' ' + ocorrencia.hora + '</b> será apagada.')
+        .ariaLabel('apagar ocorrencia')
+        .targetEvent(ev)
+        .ok('Sim')
+        .cancel('Cancelar');
       $mdDialog.show(confirm).then(function() {
         var aux = ocorrencia.data;
-        ocorrencia.data = aux.substring(6,10) + '-' + aux.substring(3,5) + '-' + aux.substring(0,2);
+        ocorrencia.data = aux.substring(6, 10) + '-' + aux.substring(3, 5) + '-' + aux.substring(0, 2);
         ocorrencia.hora += ':00'
 
         api.historicoOcorrencia.deletarOcorrencia.delete({
-          'data': ocorrencia.data,
-          'hora': ocorrencia.hora,
-          'ra': ocorrencia.raAluno
-          }, 
+            'data': ocorrencia.data,
+            'hora': ocorrencia.hora,
+            'ra': ocorrencia.raAluno
+          },
           // Sucesso
           function(response) {
             console.log(response);
