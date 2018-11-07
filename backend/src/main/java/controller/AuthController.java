3decf82e7f03e1de852b54f65c76fc57af3c4417
feluz <felipe_feluz@hotmail.com>
2018-11-07 06:58:20 -0200

    implementando Refresh Token

---
+++ backend/src/main/java/controller/AuthController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import security.JWTUtil;
+import security.UserSS;
+import security.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthController {
+ 	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+}
