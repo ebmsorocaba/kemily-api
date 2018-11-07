1690e36d4320ded3b0c7801809d3ac69456c0aec
feluz <felipe_feluz@hotmail.com>
2018-11-07 04:03:13 -0200

    Implementando autorização por tokens JWT

--- backend/src/main/java/config/SecurityConfig.java
+++ backend/src/main/java/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import security.JWTAuthenticationFilter;
+import security.JWTAuthorizationFilter;
 import security.JWTUtil;
 
 @Configuration
@@ -48,6 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
--- backend/src/main/java/kemily/app/KemilyApplication.java
+++ backend/src/main/java/kemily/app/KemilyApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class)
+@SpringBootApplication//(exclude = org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class)
 @ComponentScan("controller") //Aqui se coloca o package para scanear os controllers
 @ComponentScan("Config")
 @ComponentScan("Security")
